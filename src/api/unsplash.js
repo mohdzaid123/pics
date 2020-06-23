500709fd19c7a59f7cb088317907c76ec27a6e89
mohd-zaid <zaidmohd130399@gmail.com>
2020-06-23 22:29:03 +0530

    intial

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID EuxP16GCSU60HrEQO4Sf8EyecF36ooox_ocs273W_e8",
+    Authorization: "",
   },
 });
